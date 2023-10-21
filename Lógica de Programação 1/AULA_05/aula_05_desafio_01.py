e4f8e1badf6d38b055095dee1f1f4210d946fd65
phvidal <vidal081089@gmail.com>
2023-10-20 22:06:30 -0300

    inicio resolução desafios aula 05

---
+++ Lógica de Programação 1/AULA_05/aula_05_desafio_01.py
@@ -0,0 +1,37 @@
+
+"""
+DESAFIO
+Escreva um programa em Python usando loop for que solicita ao usuário inserir uma sequência de números inteiros e guarde todo os valores em uma lista chamada principal. 
+O programa deve continuar pedindo números até que o usuário escolha parar digitando 0. 
+Após o usuário escolher parar, o programa deve percorrer a lista principal e separar os elementos da seguinte forma:
+
+Se o elemento for negativo, use continue para voltar ao loop for
+Se o elemento é par e positivo, adicionar o elemento numa lista chamada par
+Se o elemento é impar e positivo, adicionar o elemento numa lista chamada impar
+No final, o programa deve imprimir a lista principal a lista par e a lista impar
+
+No final o programa deve imprimir a lista principal, a lista par e a lista impar.
+Obs.: Explore os recursos de controle de Laço
+
+"""
+# minha tentativa:
+# numero_usr = int(input('Digite um número para armazenar na lista, ou 0 para encerrar: '))
+# lista_usuario = []
+
+# while numero_usr != 0:
+#     lista_usuario.append(numero_usr)
+#     numero_usr = int(input('Digite um número para armazenar na lista, ou 0 para encerrar: '))
+
+# print(lista_usuario)
+
+# par = []
+# impar = []
+
+# for a, b in enumerate(lista_usuario):
+#     if a % 2 == 0:
+#         par.append(b)
+#     print(a, b)
+#     print(par)
+
+
+
