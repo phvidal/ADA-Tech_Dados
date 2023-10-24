4b2ac1094464a674ca1abb6c7f7866f826553625
phvidal <vidal081089@gmail.com>
2023-10-24 13:43:29 -0300

    Inicio projeto final - modulo 1

---
+++ Lógica de Programação 1/Projeto_Final_modulo_1/Chatbot_Pizzaria_Delicia.py
@@ -0,0 +1,20 @@
+"""
+Imagine que você está criando um chatbot para a Pizzaria que Delícia, onde o funcionamento da pizzaria é de 17h às 3h da manhã. 
+Os clientes podem fazer pedidos de pizzas disponíveis. O chatbot interagirá com os clientes, perguntando o sabor da pizza desejada e confirmará o preço, 
+caso a pizza escolhida esteja no menu. O chatbot também será capaz de lidar com pedidos de múltiplas pizzas, calcular o valor total do pedido e fornecer uma 
+lista final de pizzas pedidas e o valor total.
+
+*Carregue o menu abaixo em uma lista*
+
+Menu com pizzas disponíveis:
+
+| Nome da pizza | Preço |
+| ------------- | ----- |
+| Calabresa     | 70 |
+| Queijo        | 50 |
+| Pepperoni     | 80 |
+| Margherita    | 80 |
+| Vegetariana   | 80 |
+
+"""
+
